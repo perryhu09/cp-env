8af422eb4342c3016079e3dbe03910ebdc1fe123
Perry Hu <87783674+perryhu09@users.noreply.github.com>
2024-10-05 10:37:19 -0400

    Update template.cpp

--- templates/template.cpp
+++ templates/template.cpp
@@ -16,7 +16,7 @@ int solve(){
 int32_t main(){
     ios_base::sync_with_stdio(0); 
     cin.tie(0);
-    int tc = 1e9; 
+    int tc = 1; 
     //cin >> tc;
     for(int i = 1; i <= tc; i++){
         if(solve()){
