9b6d7ed5fab2c3efbffc930d3d0c32b6745b992e
Perry Hu <87783674+perryhu09@users.noreply.github.com>
2024-10-07 19:05:57 -0400

    Add files via upload

---
+++ templates/usaco_template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+using pii = pair<int, int>;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    freopen("test.in", "r", stdin); freopen("test.out", "w", stdout);
+    
+    return 0;
+}
