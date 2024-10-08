6085dd16d312a9693dd88e9fb5cc366af7c61179
Perry Hu <87783674+perryhu09@users.noreply.github.com>
2024-10-08 19:41:14 -0400

    Update usaco_template.cpp

--- templates/usaco_template.cpp
+++ templates/usaco_template.cpp
@@ -2,11 +2,20 @@
 using namespace std;
 
 typedef long long ll;
+
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+
 using pii = pair<int, int>;
+#define f first
+#define s second
+#define mp make_pair
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
-    freopen("test.in", "r", stdin); freopen("test.out", "w", stdout);
-    
+    freopen("test.in", "r", stdin);
+    //freopen("test.out", "w", stdout);
+
     return 0;
 }
